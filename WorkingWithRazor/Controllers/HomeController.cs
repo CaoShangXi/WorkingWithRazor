77d165af6bbe9e0c2753016e3aa21eb177450a59
CaoShangXi <1427026071qq@sina.com>
2020-05-08 00:00:23 +0800

    项目初始化

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string[] names = { "Apple","Orange","Pear"};
+            return View(names);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ WorkingWithRazor/Infrastructure/CustomLocationViewEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Infrastructure
+{
+    public class CustomLocationViewEngine:RazorViewEngine
+    {
+        public CustomLocationViewEngine()
+        {
+            //配置视图搜索的位置，{0}表示视图名，{1}表示控制器名，{2}表示区域名
+            ViewLocationFormats = new string[] { "~/Views/{1}/{0}.cshtml","~/Views/Common/{0}.cshtml"};
+        }
+    }
+}
